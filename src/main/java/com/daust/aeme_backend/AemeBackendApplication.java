b8fa5a1195a099b09b83c0ba1a695619a1f1ca82
Tidiane <tidianecs20pgmail.com>
2026-02-24 00:31:45 +0000

    Init Project

---
+++ src/main/java/com/daust/aeme_backend/AemeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.daust.aeme_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AemeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AemeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daust/aeme_backend/AemeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daust.aeme_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AemeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
