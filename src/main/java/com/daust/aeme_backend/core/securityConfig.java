6f78521858e16051e1e3a0098282eba875bce5d9
Tidiane <tidianecs20pgmail.com>
2026-02-25 05:16:04 +0000

    Done Config Keycloak locally

---
+++ src/main/java/com/daust/aeme_backend/core/securityConfig.java
@@ -0,0 +1,27 @@
+package com.daust.aeme_backend.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/public/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth -> 
+                oauth.jwt(jwt -> {})
+            );
+
+        return http.build();
+    }
+}
